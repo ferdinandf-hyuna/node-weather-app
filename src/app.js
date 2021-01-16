77bda282104e265316faad8acb1f2b5cddfdc471
ferdinandf-hyuna <ferdinand.farmer@hyuna.bb>
2021-01-16 17:46:08 -0400

    Configure app to push to Heroku

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const forecast = require('./utils/forecast')
 const { createSecretKey } = require('crypto')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 const address = process.argv[2]
 
@@ -86,6 +87,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000')
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
 })
